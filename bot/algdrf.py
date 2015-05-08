7b30427f16ea395d82d6d3dfa0b4faba81813508
Kotaro Terada <kotaro@tribox.jp>
2015-05-09 01:41:30 +0900

    Generated first html.

--- bot/algdrf.py
+++ bot/algdrf.py
@@ -891,7 +891,85 @@ def random_post():
 
 # 手順一覧のHTMLを生成する
 def gen_html():
-    print("test")
+    html = """<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@DRFbot の コーナー3-cycle (DRFバッファ) 手順一覧</title>
+    <meta name="description" content="Buffer is DRF!! @DRFbot の コーナー3-cycle (DRFバッファ) 手順一覧">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+      body {
+        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+      }
+    </style>
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Buffer is DRF!!</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="https://twitter.com/DRFbot">@DRFbot</a></li>
+            <li><a href="https://github.com/kotarot/drf-bot">Fork me on GitHub</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <h1>Buffer is DRF!!</h1>
+      <p><a href="https://twitter.com/DRFbot">@DRFbot</a> は Twitter上のbotです。定期的にDRFバッファのコーナー3-cycle手順を画像付きでつぶやきます。</p>
+
+      <h3>遊び方</h3>
+      <p>例えば、「@DRFbot DRF DFL RUF」、「@DRFbot DFL RUF」、または「@DRFbot ルービックキューブ 世界記録」みたいにリプライを飛ばしてみてください。</p>
+
+      <h3>コーナー3-cycle手順一覧</h3>
+      <p>現在のコーナー3-cycle (DRFバッファ) 手順一覧です。もっと良い手順を知ってたり思いついたらリプライで知らせてください。</p>
+
+      <table class="table table-condensed">
+        <thead>
+          <tr><th>Cycle</th><th>手順</th><th>分類</th></tr>
+        </thead>
+        <tbody>
+          %s
+        </tbody>
+      </table>
+
+    </div><!-- /.container -->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+  </body>
+</html>"""
+
+    table = ""
+    for x in range(0, 24):
+        for y in range(0, 24):
+            a = alg[(x, y)][0]
+            if a[0][0] != "X":
+                table = table + "<tr><td>(DRF %s %s)</td><td>%s</td><td>%s</td></tr>" % (cornerss[x], cornerss[y], a[0], a[1])
+
+    htmlfilename = "%s/index.html" % (os.environ.get("PATH_TO_DRFBOT"))
+    print("htmlfilename: ", htmlfilename)
+    f = open(htmlfilename, "w")
+    f.write(html % (table))
+    f.close()
 
 
 # 単体実行では標準出力に表示
@@ -905,7 +983,7 @@ if __name__ == '__main__':
     print("[Info] argc: ", argc)
     if (1 < argc) and (argvs[1] == "--random-post"):
         random_post()
-    elif (1 < argc) and (argvs[2] == "--gen-html")
+    elif (1 < argc) and (argvs[1] == "--gen-html"):
         gen_html()
     else:
         show_all()
